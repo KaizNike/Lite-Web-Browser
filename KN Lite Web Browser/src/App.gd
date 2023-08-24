cd3441d93bc874700caa04adacf1bfa79ce2fd29
KaizNike <62921588+KaizNike@users.noreply.github.com>
2023-08-23 22:49:55 -0500

    More Searching + Anims
    DDG, Wiki, AO3 for search
    
    to add: find

--- KN Lite Web Browser/src/App.gd
+++ KN Lite Web Browser/src/App.gd
@@ -2,6 +2,7 @@ extends Node
 
 var paths = []
 var pathIndex = 0
+var currentSearch = "DDG"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,12 +10,8 @@ var pathIndex = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	paths.append("https://godotengine.org")
-	var error = $HTTPRequest.request(paths[pathIndex])
-	$UI/VSplitContainer/HBoxContainer/LineEdit.text = "https://godotengine.org"
-	print(error)
-	pass # Replace with function body.
-
+	var init = "https://godotengine.org"
+	_on_LineEdit_text_entered(init)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -31,13 +28,16 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	if response_code != 200:
 		$UI/VSplitContainer/TabContainer/Page.text = "Error: " + str(response_code)
 		$UI/VSplitContainer/TabContainer/HTML.text = "Error: " + str(response_code)
+#		return("HTTP Response: " + str(response_code))
 		print(response_code)
+		$UI/VSplitContainer/HBoxContainer/ProgressText/ProgressAnim.play("failed")
 	else:
 		var html = body.get_string_from_utf8()
 		$UI/VSplitContainer/TabContainer/HTML.text = html
 		var search = $BingHelp.extract_text_and_links(html)
 		$UI/VSplitContainer/TabContainer/Page.text = str(search)
 		print("Loaded")
+		$UI/VSplitContainer/HBoxContainer/ProgressText/ProgressAnim.play("loaded")
 		pathIndex += 1
 #		html = "<p>This is some text</p> aaa <p>This is some other text</p>"
 #		var regex = RegEx.new()
@@ -55,13 +55,14 @@ func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 
 func _on_LineEdit_text_entered(new_text):
 	print("Trying connection")
+	$UI/VSplitContainer/HBoxContainer/ProgressText/ProgressAnim.play("loading")
 	var request = $HTTPRequest.request(new_text)
 	if request != OK:
 		$UI/VSplitContainer/TabContainer/Page.text = "Error: " + str(request)
 		$UI/VSplitContainer/TabContainer/HTML.text = "Error: " + str(request)
+		$UI/VSplitContainer/HBoxContainer/ProgressText/ProgressAnim.play("failed")
 		print(request)
 	paths.append(new_text)
-	
 	pass # Replace with function body.
 
 
@@ -70,17 +71,29 @@ func _on_Button_pressed():
 
 
 func _on_Button3_pressed():
-	print("Searching with DDG! Quack.")
 	var query = $UI/VSplitContainer/HBoxContainer/LineEdit.text.http_escape()
-	var search_term = "https://duckduckgo.com/?q=" + query# percent_encode()
+	var search_term = ""
+	if currentSearch == "DDG":
+		print("Searching with DDG! Quack.")
+		search_term = "https://duckduckgo.com/?q=" + query# percent_encode()
+	elif currentSearch == "Wiki":
+		print("Looking it up on Wiki Wiki-pedia")
+		search_term = "https://en.wikipedia.org/wiki/" + query
+	elif currentSearch == "AO3":
+		print("Reading about it on A03")
+		search_term = "https://archiveofourown.org/works/search?work_search%5Bquery%5D=" + query
+	else:
+		$UI/VSplitContainer/TabContainer/Page.text = "No search selected."
+		$UI/VSplitContainer/TabContainer/HTML.text = "No html from search not selected."
 #	var search_term = "https://api.duckduckgo.com/?q=" + query + "&format=html"
 #	paths.append(search_term)
 	$UI/VSplitContainer/HBoxContainer/LineEdit.text = search_term
 	_on_LineEdit_text_entered(search_term)
 
-
+# Cancel
 func _on_Button4_pressed():
 	$HTTPRequest.cancel_request()
+	$UI/VSplitContainer/HBoxContainer/ProgressText/ProgressAnim.play("cancel")
 	pass # Replace with function body.
 
 # Back
@@ -95,4 +108,34 @@ func _on_Button6_pressed():
 	if pathIndex != paths.size() - 1:
 		pathIndex += 1
 		$UI/VSplitContainer/HBoxContainer/LineEdit.text = paths[pathIndex]
+	pass # Replace with function body.
+
+# DDG
+func _on_CheckBox_toggled(button_pressed):
+	if button_pressed:
+		$UI/VSplitContainer/HBoxContainer/CheckBox2.pressed = false
+		$UI/VSplitContainer/HBoxContainer/CheckBox3.pressed = false
+		currentSearch = "DDG"
+	else:
+		currentSearch = ""
+	pass # Replace with function body.
+
+# Wiki
+func _on_CheckBox2_toggled(button_pressed):
+	if button_pressed:
+		$UI/VSplitContainer/HBoxContainer/CheckBox.pressed = false
+		$UI/VSplitContainer/HBoxContainer/CheckBox3.pressed = false
+		currentSearch = "Wiki"
+	else:
+		currentSearch = ""
+	pass # Replace with function body.
+
+
+func _on_CheckBox3_toggled(button_pressed):
+	if button_pressed:
+		$UI/VSplitContainer/HBoxContainer/CheckBox.pressed = false
+		$UI/VSplitContainer/HBoxContainer/CheckBox2.pressed = false
+		currentSearch = "AO3"
+	else:
+		currentSearch = ""
 	pass # Replace with function body.
